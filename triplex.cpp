3e0ff2ff32b7a66079c5681d062e94836831c224
Guillermo Riaño <guilledark17@gmail.com>
2023-01-14 14:41:30 -0600

    ✅ Adding Tests: Class 17

--- triplex.cpp
+++ triplex.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main()
-{
-    std::cout << "Hello, World!";
+{    
+    std::cout << "You are secret agent breaking into a secure server room.";  //Show text in terminal
+    std::cout << std::endl; //line break
+    std::cout << "You need to enter the correct codes to continue...";
     return 0;
 }
