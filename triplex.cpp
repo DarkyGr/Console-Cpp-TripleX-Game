2f0aeafed21ea1b644021a0a7cdef4800e324d22
Guillermo Riaño <guilledark17@gmail.com>
2023-01-14 22:59:11 -0600

    ✅ Adding Tests: Functions

--- triplex.cpp
+++ triplex.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
-int main()
-{   
+void PrintIntroduction(){
     //Message Start
-    std::cout << "You are secret agent breaking into a secure server room.";  //Show text in terminal
-    std::cout << std::endl; //line break
-    std::cout << "You need to enter the correct codes to continue..." << std::endl;
+    std::cout << "You are secret agent breaking into a secure server room.\n";  //Show text in terminal    
+    std::cout << "You need to enter the correct codes to continue...\n\n";
+}
 
+void PlayGame(){
     
+    PrintIntroduction();
+
     //Declare 3 variables
     const int CodeA = 4;
     const int CodeB = 3;
@@ -16,31 +18,33 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    //Print sum and product to the terminal
+    //Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct;
     
-
-    //Getting user input
+    //Store player guess
     int GuessA , GuessB, GuessC;
-    std::cin >> GuessA >> GuessB >> GuessC;    //Store text
-    std::cout << "+ You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+    std::cout << std::endl; //line break
+    std::cin >> GuessA >> GuessB >> GuessC;    
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    //Check if the player guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You win!" << std::endl;
-        std::cout << "+ The guess and codes add-up to: " << GuessSum << std::endl;
-        std::cout << "+ The guess and codes multiply to give: " << GuessProduct << std::endl;
+        std::cout << "\nYou win!";
     }else
     {
-        std::cout << "You lose!" << std::endl;
-    }
-    
+        std::cout << "\nYou lose!";
+    }    
+}
+
+int main()
+{    
+    PlayGame();
 
     return 0;
 }
