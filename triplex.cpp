7a24d1f192493e76f5a8af8c1dc9adc6bf3d5422
Guillermo Riaño <guilledark17@gmail.com>
2023-01-14 22:30:01 -0600

    ✅ Adding Tests: Naming & Self Documenting Code

--- triplex.cpp
+++ triplex.cpp
@@ -5,18 +5,23 @@ int main()
     //Message Start
     std::cout << "You are secret agent breaking into a secure server room.";  //Show text in terminal
     std::cout << std::endl; //line break
-    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    //Declare 3 variables
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
 
-    const int sum = a + b +c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
+    //Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int PlayerGuess;
 
     return 0;
 }
